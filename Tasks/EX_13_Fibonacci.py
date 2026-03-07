f577b2836ea2f837e355589eb2a6ca92d453adf6
Levan Jinjolava <59836200+Pip-Squeak@users.noreply.github.com>
2026-03-07 19:07:49 +0400

    Add Fibonacci number generator program

--- Tasks/EX_13_Fibonacci.py
+++ Tasks/EX_13_Fibonacci.py
@@ -5,7 +5,7 @@ Write a program that asks the user how many Fibonnaci numbers to generate
 and then generates them. Take this opportunity to think about how you can use functions.
 Make sure to ask the user to enter the number of numbers in the sequence to generate.
 """
-# Asking user for a amount of Fibonnaci number
+# Asking user for an amount of Fibonnaci number
 def users_fibonnaci (fibonnaci_number = 9):
     fibonnaci_number = int(input("\nHow many Fibonnaci numbers to generate: "))
     return fibonnaci_number
@@ -22,3 +22,4 @@ def fibonnaci_generator():
     return result
 
 print(fibonnaci_generator())
+
